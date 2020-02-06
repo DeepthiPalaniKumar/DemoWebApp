0f69bc27d0922fbf59cf7b308fde1379c08dd0e0
DeepthiPalaniKumar <59816043+DeepthiPalaniKumar@users.noreply.github.com>
2020-02-06 11:55:09 +0530

    Change by commenting in github to trigger a build

--- WebDemo2/Program.cs
+++ WebDemo2/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//To trigger a auto build when changed in github after git is made as the source
 namespace WebDemo2
 {
     public class Program
